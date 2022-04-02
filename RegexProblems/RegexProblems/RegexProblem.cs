c0431f03e4e62918a280c1dda3541144a8c136cc
sanjay <ssah6694@gmail.com>
2022-04-02 07:02:17 +0530

    [sanjay]Added: First name File

---
+++ RegexProblems/RegexProblems/RegexProblem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblems
+{
+    public class RegexProblem
+    {
+        public static void Firstname()
+        {
+            Console.WriteLine("Please Enter your First name");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Regex r = new Regex("^[A-Z][a-z]{2,}$");
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("your First name is " + true);
+                }
+                else
+                {
+                    Console.WriteLine("Your First name is " + false);
+                    Console.WriteLine("First Name Should be Contain Only Four Character");
+                }
+            }
+        }
+    }
+}
