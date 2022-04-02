3b9f3daea0c9ff44784479aabccb117e5b4b4bd9
sanjay <ssah6694@gmail.com>
2022-04-02 07:36:41 +0530

    [sanjay]Added: Password with upper case File

--- RegexProblems/RegexProblems/RegexProblem.cs
+++ RegexProblems/RegexProblems/RegexProblem.cs
@@ -9,14 +9,14 @@ namespace RegexProblems
 {
     public class RegexProblem
     {
-        public static void Password()
+        public static void PasswordWitUpperCase()
         {
             Console.WriteLine("Please Enter your password");
 
             while (true)
             {
                 string input = Console.ReadLine();
-                Regex r = new Regex("^[A-Za-z0-9]{8}$");
+                Regex r = new Regex("^[A-Z][a-z]{7}$");
                 if (r.IsMatch(input))
                 {
                     Console.WriteLine("your password is " + true);
